d61c3c779732215c7d5e82fcd4bd67a2135f3fad
Rimi Nishida <184547242+innRy@users.noreply.github.com>
2025-08-28 00:23:55 +0900

    footer

--- ise_warabi/test/src/main.jsx
+++ ise_warabi/test/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import App from './App.jsx'
 import Header from './header.jsx' 
+import Footer from './footer.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
     <App />
+    <Footer/>
   </StrictMode>,
 )
