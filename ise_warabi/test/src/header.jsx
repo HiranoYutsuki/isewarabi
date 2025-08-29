fca5f881d0983163e0b6c82638379b3cc504c113
HiranoYutsuki <241205150@ie.meijo-u.ac.jp>
2025-08-30 00:19:55 +0900

    コンフリクトは嫌だ

--- ise_warabi/test/src/header.jsx
+++ ise_warabi/test/src/header.jsx
@@ -1,18 +1,22 @@
 // Header.jsx 側
-function Header({ onShowQRScan }) {
+function Header({ onShowQRScan, onShowStampPage }) {
     return (
         <header>
-        <h1>isewarabi</h1>
-        <nav className="header-nav">
-            <ul>
-            <li>
-                <a href="#" onClick={(e) => { e.preventDefault(); onShowQRScan(); }}>
-                QR読み取り
-                </a>
-            </li>
-            <li><a href="#score-list">スコア/スタンプ一覧</a></li>
-            </ul>
-        </nav>
+            <h1>isewarabi</h1>
+            <nav className="header-nav">
+                <ul>
+                    <li>
+                        <a href="#" onClick={(e) => { e.preventDefault(); onShowQRScan(); }}>
+                            QR読み取り
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" onClick={(e) => { e.preventDefault(); onShowStampPage(); }}>
+                            スコア/スタンプ一覧
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
