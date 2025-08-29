1f99a95f80c019876c269db62be2454d4bae4382
Rimi Nishida <184547242+innRy@users.noreply.github.com>
2025-08-30 00:15:54 +0900

    Update App.jsx

--- ise_warabi/test/src/App.jsx
+++ ise_warabi/test/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState, useRef, useEffect } from 'react'
 import StampPage from './components/StampPage'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import jsQR from 'jsqr'
 import './css/App.css'
 
